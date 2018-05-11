8198b26d00eef271235311165a899c19102674b5
Smythy <smythy.costa@hotmail.com>
2018-05-11 13:46:47 -0300

    Routs api category

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function (){
+    Route::get('/categories', 'CategoryController@index');
+    Route::post('/categories', 'CategoryController@store');
+    Route::put('/categories/{category}', 'CategoryController@update');
+    Route::get('/categories/{category}', 'CategoryController@show');
+    Route::delete('/categories/{category}', 'CategoryController@destroy');
+});
